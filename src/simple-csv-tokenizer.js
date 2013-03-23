f928133173023464ec7963b77f9d4a324ef4fd2b
david kaye <dfkaye@yahoo.com>
2013-03-22 23:48:45 -0700

    some comment cleanup

--- src/simple-csv-tokenizer.js
+++ src/simple-csv-tokenizer.js
@@ -3,8 +3,7 @@
  * @author David Kaye (@dfkaye)
  * @date 2013-3-19
  *
- * No regex, no iterator, just simple one-character-at-a-time parsing of possible csv input string.
- * Calls itself on subsequent substrings.
+ * No regex, no iterator method, just one-character-at-a-time parsing of possible csv input string.
  *
  * Some expectations of the input (taken from http://www.csvreader.com/csv_format.php):
  *
@@ -21,7 +20,6 @@
  *
  *  To guarantee preservation of leading and trailing whitespace characters, a field must be text 
  *      qualified by embedding the field inside a set of double quotes.
- *
  */
 
 ;(function (exports) {
